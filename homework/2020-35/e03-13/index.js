ff15117384f71ed0341edee6e0273979318f4b01
PetriIrri <petri.irri@gmail.com>
2021-09-02 12:31:26 +0300

    done exercise 5

--- homework/2020-35/e03-13/index.js
+++ homework/2020-35/e03-13/index.js
@@ -1,5 +1,35 @@
 var readlineSync = require("readline-sync");
-
-// Wait for user's response.
-var userName = readlineSync.question("May I have your name? ");
-console.log("Hello " + userName + "!");
+var choice = readlineSync.question(
+  "What exercise would you like to try? This folder has exercises 3-13. -->"
+);
+//exercise 3
+if (choice == 3) {
+  console.log(
+    "Create an application that asks your name. Then output the given name. Use readline-sync.\n"
+  );
+  // Wait for user's response.
+  var userName = readlineSync.question("May I have your name? ");
+  console.log("Hello " + userName + "!");
+} else if (choice == 4) {
+  console.log(
+    "Ask also a number, output the name number of times. So if given “jack” and 4, output jackjackjackjack. (google for / while javascript. Or string repeat)\n"
+  );
+  // Exercise 4
+  // Wait for user's response.
+  var Name = readlineSync.question("May I have your name? ");
+  var number = readlineSync.question(
+    "How many times would you like for me to repeat that? "
+  );
+  console.log(Name.repeat(number));
+} else if (choice == 5) {
+  console.log(
+    "Create variable containing some string, for example ‘hello’. Try to cast it to number using Number - function. What do you get when this fails? Transform the result to string. Repeat the string 16 times and output it. Output also ‘batman’. 🙂 \n"
+  );
+  let text = "hallo!";
+  console.log("sample text: " + text);
+  text = Number(text);
+  console.log(text);
+  console.log("transforming back to string and repeating 16 times!");
+  text = String(text);
+  console.log(text.repeat(16) + " batman");
+}
--- homework/2020-35/e03-13/index2.js
+++
@@ -1,9 +0,0 @@
-// Exercise 4
-var readlineSync = require("readline-sync");
-
-// Wait for user's response.
-var userName = readlineSync.question("May I have your name? ");
-var number = readlineSync.question(
-  "How many times would you like for me to repeat that? "
-);
-console.log(userName.repeat(number));
