95d4dd7a2cd59fa787eda7bb89fe6c8a90d60352
PetriIrri <petri.irri@gmail.com>
2021-09-15 16:28:15 +0300

    done exercise 21

---
+++ homework/2020-37/e18-e22/e21.js
@@ -0,0 +1,11 @@
+let user = {
+  name: "Jack",
+  sayHello: function () {
+    console.log(`Hello, ${this.name}!`);
+  },
+  sayDelayedHello: function () {
+    let f = this.sayHello.bind(user);
+    setTimeout(f, 1000);
+  },
+};
+user.sayDelayedHello();
