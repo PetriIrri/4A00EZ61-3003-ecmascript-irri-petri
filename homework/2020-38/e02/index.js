3cf0a4a4085f67a5434a577b0c2709d8dab5c426
Petri Irri <petri.irri@tuni.fi>
2021-09-22 11:28:41 +0300

    a

---
+++ homework/2020-38/e02/index.js
@@ -0,0 +1,4 @@
+import { max, min } from "./modules/utility.js";
+let myarray = [11, 22, 33, 44, 55];
+console.log(max(myarray));
+console.log(min(myarray));
---
+++ homework/2020-38/e02/modules/utility.js
@@ -0,0 +1,8 @@
+const max = (array) => {
+  return Math.max.apply(null, array);
+};
+const min = (array) => {
+  return Math.min.apply(null, array);
+};
+
+export { max, min };
