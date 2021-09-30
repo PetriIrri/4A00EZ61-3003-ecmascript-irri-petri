8d0f5f43d75f2b150f4a813dbbdb7db7e99ec1cd
PetriIrri <petri.irri@gmail.com>
2021-09-30 10:29:35 +0300

    Add done exercise 7 week 39

---
+++ homework/2020-39/e07/index.js
@@ -0,0 +1,33 @@
+const util = require("util");
+const readlineSync = require("readline-sync");
+const fs = require("fs");
+const readFile = util.promisify(fs.readFile);
+
+function parseJson(data) {
+  function func(resolve, reject) {
+    // parse the data (do not implement try catch, if exception occurs, this
+    // will be catched automatically in your catch outside of this function.
+    // if name is found from obj, call resolve, otherwise call reject
+    const obj = JSON.parse(data);
+    if (!("name" in obj)) {
+      reject("Json file does not contain property name");
+    } else {
+      resolve(obj.name);
+    }
+  }
+  const p = new Promise(func);
+  return p;
+}
+
+async function readFileAndParse(fileName) {
+  const result = await readFile(fileName, "utf-8");
+  const name = await parseJson(result);
+
+  return name;
+}
+
+const fileName = readlineSync.question("Give a file name: ");
+
+readFileAndParse(fileName)
+  .then((name) => console.log(name))
+  .catch((msg) => console.log(msg));
